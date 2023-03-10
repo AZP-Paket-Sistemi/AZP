a9e1c84229765ff1e3b7ff3899be30714f2ca02a
AZP <91795411+AZP-Paket-Sistemi@users.noreply.github.com>
2023-03-10 14:10:57 +0400

    AZP 4.0

---
+++ azp-install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+### AZP Paket Sistemi 4.0
+# AZP Offical Site: https://azp-paket-sistemi.github.io
+# AZP Offical Depo: https://azp-paket-sistemi.github.io/azp-depo
+
+# © 2021-2023 AZP
+# © 2019-2023 PacPERRO
+
+# Rənglər
+error=$(echo -e "\033[1;31m E: \033[:0m")
+process=$(echo -e "\033[1;34m P: \033[:0m")
+finished=$(echo -e "\033[1;32m F: \033[:0m")
+info=$(echo -e "\033[1;36m I: \033[:0m")
+
+TEXTDOMAINDIR=/usr/share/locale
+TEXTDOMAIN=azp-install
+cp -r locale/az/LC_MESSAGES/azp-*.mo /usr/share/locale/az/LC_MESSAGES
+cp -r locale/tr/LC_MESSAGES/azp-*.mo /usr/share/locale/tr/LC_MESSAGES
+
+# Root kontrol
+if [ $UID -ne 0 ]
+then
+  echo "$info" $"Command: sudo ./azp-install.sh"
+  exit
+fi
+
+files="usr/bin/azp*
+       usr/share/applications/azp-about.desktop
+       usr/share/azp
+       usr/share/pixmaps/azp*
+       usr/share/locale/*/LC_MESSAGES/azp*.mo"
+
+
+echo -e """\033[1;34m
+
+   _________________________________
+   |              ___________      |
+   |       /\    |___  /  __ \     |
+   |      /  \      / /| |__) |    |
+   |     / /\ \    / / |  ___/     |
+   |    / ____ \  / /__| |         |
+   |   /_/    \_\/_____|_|         |                               
+    \                             /
+     -----------------------------
+
+                
+           <<  || 4.0 || >>
+                -------
+"""
+sleep 0.5
+echo "$process" $"AZP install..."
+tar -xf src.tar.gz
+for i_f in $files;
+do
+  echo "$process" $"Copy AZP file"" == ${i_f} >> /${i_f}"
+  if ! cp -prf ${i_f} /${i_f} &>/dev/null;
+  then
+    echo "$error" $"Failed copy files"" == /${i_f}"
+    exit 1
+  fi
+done
+xdg-mime install azp-mimes.xml
+xdg-icon-resource install --context mimetypes --size 64 azp.png application-x-azp
+xdg-icon-resource install --context mimetypes --size 64 azp.png application-x-src-azp
+xdg-icon-resource install --context mimetypes --size 64 azp.png application-x-azp-script
+azp -add-architecture -auto
+echo "$finished" $"AZP install finished"
+rm -r /usr/share/locale/*/LC_MESSAGES/azp-install.mo
+rm -r usr
---
+++ azp-remove.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+### AZP Paket Sistemi 4.0
+# AZP Offical Site: https://azp-paket-sistemi.github.io
+# AZP Offical Depo: https://azp-paket-sistemi.github.io/azp-depo
+
+# © 2021-2023 AZP
+# © 2019-2023 PacPERRO
+
+# Rənglər
+error=$(echo -e "\033[1;31m E: \033[:0m")
+process=$(echo -e "\033[1;34m P: \033[:0m")
+finished=$(echo -e "\033[1;32m F: \033[:0m")
+remove=$(echo -e "\033[1;31m R: \033[:0m")
+info=$(echo -e "\033[1;36m I: \033[:0m")
+
+TEXTDOMAINDIR=/usr/share/locale
+TEXTDOMAIN=azp-remove
+
+# Root kontrol
+if [ $UID -ne 0 ]
+then
+  echo "$info" $"Command: sudo ./azp-remove.sh"
+  exit
+fi
+
+files="/usr/bin/azp*
+       /usr/share/applications/azp-about.desktop
+       /usr/share/azp
+       /usr/share/pixmaps/azp*.png
+       /usr/share/locale/*/LC_MESSAGES/azp*.mo"
+
+echo "$info" $"Program data folders will not be deleted when uninstalling azp"
+sleep 1
+echo "$remove" $"AZP remove..."
+for r_f in $files;
+do
+  echo "$remove" $"Remove AZP file"" == ${r_f}"
+  if ! rm -r ${r_f} &>/dev/null;
+  then
+    echo "$error" $"Failed remove files"" == ${r_f}" &>/dev/stderr
+    exit 1
+  fi
+done
+rm -r /etc/azp
+rm -r /var/cache/azp-archives
+rm -r /var/cache/azp/azp-depo /var/cache/azp/azp-architecture
+echo "$finished" $"AZP remove finished"
